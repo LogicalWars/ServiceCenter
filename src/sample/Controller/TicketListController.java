e5f79ffc5a28552698c7903a73f7cec389429db8
KeeperWar <rewin95@mail.ru>
2020-02-23 17:19:30 +0400

    Обработка двойного щелчка по таблице

--- src/sample/Controller/TicketListController.java
+++ src/sample/Controller/TicketListController.java
@@ -1,9 +1,12 @@
 package sample.Controller;
 
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TablePosition;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import sample.Model.DataTickets;
 import sample.Model.Tickets;
 
@@ -33,6 +36,18 @@ public class TicketListController {
         fullName.setCellValueFactory            (new PropertyValueFactory<Tickets, String>("fullName"));
         dateCreateTicket.setCellValueFactory    (new PropertyValueFactory<Tickets, String>("dateCreateTicket"));
         statusTicket.setCellValueFactory        (new PropertyValueFactory<Tickets, String>("statusTicket"));
+
+        tableTickets.setOnMousePressed(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                if (event.isPrimaryButtonDown() && event.getClickCount() == 2) {
+                    TablePosition pos = (TablePosition) tableTickets.getSelectionModel().getSelectedCells().get(0);
+                    int row = pos.getRow();
+                    TableColumn col = pos.getTableColumn();
+                    String data = (String) col.getCellObservableValue(tableTickets.getItems().get(row)).getValue();
+                    System.out.println(data);
+                }
+            } });
 
         tableTickets.setItems(dataTickets.getTicketsData());
     }
