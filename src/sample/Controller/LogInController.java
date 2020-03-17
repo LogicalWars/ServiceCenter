157cae2ab95b25ce9c4dba2f6f06c5c161475a51
LogicalWars <koshelevva96@gmail.com>
2020-03-17 20:29:41 +0400

    up

--- src/sample/Controller/LogInController.java
+++ src/sample/Controller/LogInController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -19,29 +20,33 @@ public class LogInController {
     private TextField user;
     @FXML
     private PasswordField password;
+    @FXML
+    private Label errorPass;
 
     @FXML
     public void logInButton() {
-
-        Stage stage = (Stage) user.getScene().getWindow();
-        stage.close();
-
-
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MainMenu.fxml"));
-            loader.setController(new MainMenuController());
-            Parent pane = loader.load();
-            Stage primaryStage = new Stage();
-            Scene scene = new Scene(pane);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-            primaryStage.setMinHeight(640);
-            primaryStage.setMinWidth(920);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
         User.getUserRole(user.getText(), password.getText());
+        if (User.USER != null) {
+            Stage stage = (Stage) user.getScene().getWindow();
+            stage.close();
+
+
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("../View/MainMenu.fxml"));
+                loader.setController(new MainMenuController());
+                Parent pane = loader.load();
+                Stage primaryStage = new Stage();
+                Scene scene = new Scene(pane);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+                primaryStage.setMinHeight(640);
+                primaryStage.setMinWidth(920);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }else{
+            errorPass.setVisible(true);
+        }
     }
 
 }
